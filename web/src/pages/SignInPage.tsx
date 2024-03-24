7a5749450a70ca8d96ea3b3daba1e163ec80fd6a
JhosefMatheus <jhosef.mat123@hotmail.com>
2024-03-24 18:42:37 -0300

    Add more style to sing in page form

--- web/src/pages/SignInPage.tsx
+++ web/src/pages/SignInPage.tsx
@@ -2,8 +2,16 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 export function SignInPage(): JSX.Element {
+  const [login, setLogin] = useState<string>("");
+  const [validLogin, setValidLogin] = useState<boolean>(true);
+
+  const [password, setPassword] = useState<string>("");
+  const [validPassword, setValidPassword] = useState<boolean>(true);
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+
   return (
     <div
       className={cn(
@@ -23,7 +31,30 @@ export function SignInPage(): JSX.Element {
           </Label>
           <Input
             id="login"
+            className={cn(
+              "bg-slate-700 text-white ring-offset-0 focus-visible:ring-offset-0 focus-visible:ring-0",
+              "border-transparent border-2",
+              validLogin ? "focus-visible:border-blue-500" : "border-red-500"
+            )}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const text: string = e.target.value;
+
+              setLogin(text);
+              setValidLogin(text.length > 0);
+            }}
+            onBlur={(e: React.FocusEvent<HTMLInputElement>) => {
+              const text: string = e.target.value;
+
+              setValidLogin(text.length > 0);
+            }}
           />
+          {!validLogin && (
+            <span
+              className="text-red-500 text-xs"
+            >
+              Campo obrigatório
+            </span>
+          )}
         </div>
         <div>
           <Label
@@ -34,11 +65,36 @@ export function SignInPage(): JSX.Element {
           </Label>
           <Input
             id="password"
-            type="password"
+            type={passwordVisible ? "text" : "password"}
+            className={cn(
+              "bg-slate-700 text-white ring-offset-0 focus-visible:ring-offset-0 focus-visible:ring-0",
+              "border-transparent border-2",
+              validPassword ? "focus-visible:border-blue-500" : "border-red-500"
+            )}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const text: string = e.target.value;
+
+              setPassword(text);
+              setValidPassword(text.length > 0);
+            }}
+            onBlur={(e: React.FocusEvent<HTMLInputElement>) => {
+              const text: string = e.target.value;
+
+              setValidPassword(text.length > 0);
+            }}
           />
+          {!validPassword && (
+            <span
+              className="text-red-500 text-xs"
+            >
+              Campo obrigatório
+            </span>
+          )}
         </div>
         <Button
           className="w-full bg-blue-600 hover:bg-blue-500"
+          type="submit"
+          disabled={!validLogin || !validPassword || login.length === 0 || password.length === 0}
         >
           Entrar
         </Button>
